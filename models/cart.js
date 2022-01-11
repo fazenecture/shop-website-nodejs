99b97f46daafbd2e587e39ae783270f73bcc6e77
Vivek <37245253+fazenecture@users.noreply.github.com>
2022-01-12 03:45:32 +0530

    edit-product

--- models/cart.js
+++ models/cart.js
@@ -25,14 +25,14 @@ module.exports = class Cart {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
-        cart.products[existingProductIndex]= updatedProduct;
+        cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) =>{
-          console.log(err);
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
       });
     });
   }
