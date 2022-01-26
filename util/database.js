89fa2f8bf779078b177dcd8332a955df132e73a2
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-26 06:50:54 +0530

    commit

--- util/database.js
+++ util/database.js
@@ -1,8 +1,39 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = () => {
+  MongoClient.connect(
+    "mongodb+srv://new-user-01:Jaimatadi@cluster0.3optd.mongodb.net/Shop?retryWrites=true&w=majority"
+  )
+    .then(client => {
+      console.log('Connected');
+      _db = client.db();
+    })
+    .catch((err) => {console.log(err)});
+  
+};
+
+const getDb = () =>{
+  if (_db) {
+    return _db;
+  }
+  throw 'No Database Found';
+}
+
+
+
+exports.mongoConnect = mongoConnect;
+exports.db = getDb;
+
+
